d1a795a4a475ed99f0196850e109589207ba8cfc
Yu.miao <yu.miao@hinacom.com>
2018-07-20 21:25:39 +0800

    更新db

--- hbservice-core/src/main/java/com/cmic/hbservice/domain/OrderStatus.java
+++ hbservice-core/src/main/java/com/cmic/hbservice/domain/OrderStatus.java
@@ -3,7 +3,9 @@ package com.cmic.hbservice.domain;
 public final class OrderStatus {
     public final static Integer Pending = 1;
     public final static Integer Buy = 2;
-    public final static Integer Sell = 3;
-    public final static Integer Done = 4;
-    public final static Integer Cancel = 5;
+    public final static Integer Filled = 3;
+    public final static Integer Cancel = 4;
+    public final static Integer Buy_Complete = 5;
+    public final static Integer Sell = 6;
+    public final static Integer Done = 999;
 }
