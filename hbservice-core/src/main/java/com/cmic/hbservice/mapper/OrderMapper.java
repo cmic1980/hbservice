561c14689427b605b37918227e43b6dd47a8c6a4
Yu.miao <yu.miao@hinacom.com>
2019-01-17 19:20:11 +0800

    更新db

--- hbservice-core/src/main/java/com/cmic/hbservice/mapper/OrderMapper.java
+++ hbservice-core/src/main/java/com/cmic/hbservice/mapper/OrderMapper.java
@@ -14,7 +14,7 @@ public interface OrderMapper {
             "VALUES(#{amount},#{symbol},#{orderType},#{status},#{buyPrice},#{sellPrice},#{buyTime},#{t})")
     void insert(Order order);
 
-    @Select("select *,order_item_id as orderItemId, buy_time as buyTime, order_id as orderId, buy_price as buyPrice " +
+    @Select("select *,order_item_id as orderItemId, buy_time as buyTime, order_id as orderId, buy_price as buyPrice, sell_price as sellPrice " +
             "from order_item " +
             "where status = #{status}")
     List<Order> selectListByStatus(int status);
@@ -25,7 +25,7 @@ public interface OrderMapper {
     void delete(long orderId);
 
     @Update("update order_item " +
-            "set order_id= #{orderId}, buy_price= #{buyPrice}, status= #{status} " +
+            "set order_id= #{orderId}, buy_price= #{buyPrice}, status= #{status}, sell_price=#{sellPrice} " +
             "where order_item_id = #{orderItemId}")
     void update(Order order);
 
--- hbservice-core/src/main/java/com/cmic/hbservice/service/HuobiScheduledService.java
+++ hbservice-core/src/main/java/com/cmic/hbservice/service/HuobiScheduledService.java
@@ -1,28 +1,14 @@
 package com.cmic.hbservice.service;
 
-import com.alibaba.fastjson.JSONObject;
 import com.cmic.hbservice.domain.Order;
-
 import com.cmic.hbservice.domain.OrderStatus;
-import com.huobi.api.ApiClient;
 import com.huobi.request.CreateOrderRequest;
-import com.huobi.response.Accounts;
-import com.huobi.response.AccountsResponse;
-import com.huobi.response.Kline;
-import com.huobi.response.KlineResponse;
+import com.huobi.response.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 @Component
 public class HuobiScheduledService {
@@ -44,7 +30,12 @@ public class HuobiScheduledService {
             if (date.getTime() - o.getBuyTime().getTime() > 0) {
                 var buyPrice = this.getBuyPrice(o.getSymbol());
                 buyPrice = buyPrice / 1.5;
+
                 o.setBuyPrice(buyPrice);
+                float t = ((100 + o.getT()) / 100);
+                Double sellPrice = buyPrice * t;
+                o.setSellPrice(sellPrice);
+
                 this.createBuyOrder(o);
             }
         });
@@ -82,9 +73,8 @@ public class HuobiScheduledService {
         request.accountId = String.valueOf(this.getAccountId());
         request.amount = order.getAmount().toString();
         request.price = order.getBuyPrice().toString();
-        if(request.price.length()>8)
-        {
-            request.price = request.price.substring(0,8);
+        if (request.price.length() > 8) {
+            request.price = request.price.substring(0, 8);
         }
         request.symbol = order.getSymbol();
         request.type = CreateOrderRequest.OrderType.BUY_LIMIT;
@@ -100,37 +90,96 @@ public class HuobiScheduledService {
         return orderId;
     }
 
-    /**
-     *  1. 检查挂单成交状态
-     */
-    // @Scheduled(cron = "*/5 * * * * ?")
-    public void sellOrder(){
+    private long createSellOrder(Order order) {
         var client = this.apiService.getApiClient();
 
-        var response = client.balance(this.getAccountId().toString());
+        // create order:
+        CreateOrderRequest request = new CreateOrderRequest();
+        request.accountId = String.valueOf(this.getAccountId());
+        request.amount = order.getAmount().toString();
+        request.price = order.getSellPrice().toString();
+        if (request.price.length() > 8) {
+            request.price = request.price.substring(0, 8);
+        }
+        request.symbol = order.getSymbol();
+        request.type = CreateOrderRequest.OrderType.SELL_LIMIT;
+        request.source = "api";
 
 
-        /*
-        var buyingList = this.orderService.getBuyingList();
-        buyingList.stream().forEach(o->{
-            var orderId = o.getOrderId();
-            var response = client.ordersDetail(orderId.toString());
-            response = null;
-        });*/
+        //------------------------------------------------------ 创建订单  -------------------------------------------------------
+        long orderId = client.createOrder(request);
+        order.setOrderId(orderId);
+        order.setStatus(OrderStatus.Buy);
 
-        // client.ordersDetail()
+        this.orderService.updateOrder(order);
+        return orderId;
     }
 
-    @Scheduled(cron = "*/10 * * * * ?")
-    public void cancelOrder(){
+    /**
+     * 1. 检查挂单成交状态
+     */
+    @Scheduled(cron = "*/30 * * * * ?")
+    public void sellOrder() {
         var client = this.apiService.getApiClient();
+        var response = client.balance(this.getAccountId().toString());
+        var balance = (Balance) response.getData();
+
         var buyingList = this.orderService.getBuyingList();
-        buyingList.stream().forEach(o->{
-            var orderId = o.getOrderId();
-            var response = client.submitcancel(orderId.toString());
-            o.setStatus(OrderStatus.Done);
-            this.orderService.updateOrder(o);
+
+        var itemList = (List<HashMap>) balance.getList();
+        // itemList = itemList.stream().filter(s -> s.get("balance").equals("0") == false).collect(Collectors.toList());
+        itemList.forEach(o -> {
+            String count = o.get("balance").toString();
+            String type = o.get("type").toString();
+
+            if (type.equals("trade")) {
+                // 找到匹配币种和相应剩余数量
+                var buyingOrder = buyingList.stream().filter(s -> {
+                    String symbol = s.getSymbol();
+                    symbol = symbol.replace("eth", "");
+                    String currency = o.get("currency").toString();
+                    return symbol.equals(currency);
+                }).findAny();
+
+                if (buyingOrder.isPresent()) {
+                    Order order = buyingOrder.get();
+                    order.setAmount(Float.valueOf(count));
+
+                    var ethCount = order.getAmount() * order.getSellPrice();
+                    if (ethCount > 0.0001) {
+                        this.createSellOrder(order);
+                    }
+                }
+            }
         });
 
+        response = null;
     }
+
+    /**
+     * 1. 取消订单
+     */
+    @Scheduled(cron = "*/30 * * * * ?")
+    public void cancelOrder() {
+        var client = this.apiService.getApiClient();
+        var buyingList = this.orderService.getBuyingList();
+
+        buyingList.stream().forEach(o -> {
+            var orderId = o.getOrderId();
+            // 撤销一小时后的未成交订单 （如果部分成交或全部成交调用撤销函数不会有错误，所以没判断是否成交，就是直接撤销所以买单）
+            Date date = new Date();
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            calendar.add(Calendar.HOUR, 1);
+            date = calendar.getTime();
+
+            if (date.getTime() - o.getBuyTime().getTime() > 0) {
+                var response = client.submitcancel(orderId.toString());
+                o.setStatus(OrderStatus.Done);
+                this.orderService.updateOrder(o);
+            }
+        });
+    }
+
+
 }
